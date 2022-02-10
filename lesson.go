6416d937e9d1b0d4d32960102ed45853f64eb264
磯貝　雄人 <47950027+isopets@users.noreply.github.com>
2022-02-10 23:44:27 +0900

    Go言語の練習
    変数宣言

---
+++ lesson.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var(
+	//bazz()
+	 i int = 1
+	 f64 float64 = 1.2
+	 s string = "test"
+	 t, f bool = true, false
+	)
+	fmt.Println(i, f64, s, t, f)
+
+	xi := 1
+	xf64 := 1.2
+	xs := "test"
+	xt, xf := true, false
+	fmt.Println( xi, xf64, xs, xt,xf)
+}
